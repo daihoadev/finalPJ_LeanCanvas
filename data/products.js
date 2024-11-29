20987fa3ef152a36369c052a39ac2f2c3199c2fb
daihoadev <daihoa758968@gmail.com>
2024-11-29 21:17:45 +0700

    init project

---
+++ data/products.js
@@ -0,0 +1,74 @@
+var list_products = [
+{
+    "name": "goi om 01",
+    "company": "goi om",
+    "img": "img/products/goi_om_bt.jpg",
+    "price": "350.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Cotton",
+        "os": "goi om co ban",
+        "camara": "bong gon"
+    },
+    "masp": "GO1"
+}
+,{
+    "name": "goi ngu cao cap",
+    "company": "goi ngu",
+    "img": "img/products/goi_ngu_cc.jpg",
+    "price": "1.550.000",
+    "star": 5,
+    "rateCount": 216,
+    "promo": {
+        "name": "giamgia",
+        "value": "999.000"
+    },
+    "detail": {
+        "screen": "Cotton",
+        "os": "goi ngu cao cap",
+        "camara": "Cao su non"
+    },
+    "masp": "GN1"
+},{
+    "name": "goi chu u",
+    "company": "goi chu u",
+    "img": "img/products/goi_chu_u.jpg",
+    "price": "950.000",
+    "star": 5,
+    "rateCount": 216,
+    "promo": {
+        "name": "giareonline",
+        "value": "499.000"
+    },
+    "detail": {
+        "screen": "Cotton",
+        "os": "goi ke đau chu u",
+        "camara": "cao su non"
+    },
+    "masp": "Gu1"
+},
+{
+    "name": "goi om chu u",
+    "company": "goi om chu u",
+    "img": "img/products/goi_om_u.jpg",
+    "price": "2.100.000",
+    "star": 4,
+    "rateCount": 32,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "Cotton",
+        "os": "goi om cao cap",
+        "camara": "Chat lieu tong hop"
+    },
+    "masp": "Ou1"
+},
+
+]
